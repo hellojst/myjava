917905bba925b10f18e259e800b763131de070fc
stjia <shtjia@163.com>
2018-03-18 22:17:27 +0800

    增加一个多线程的类

---
+++ myjavabase/src/ResponsiveUI.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author stjia
+ * @time 2018年3月18日
+ */
+public class ResponsiveUI extends Thread {
+	private static  double d = 1;
+	public ResponsiveUI () {
+		setDaemon(true);
+		start();
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while (true) {
+			d = d + (Math.PI + Math.E);
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		new ResponsiveUI();
+		System.in.read();
+		System.out.println(d);
+	}
+
+}
